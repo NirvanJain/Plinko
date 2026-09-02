532faa2bd0bb0ef64376bc4e6ead4cb42e2d55d3
Nirvan Jain <jainnirvan070@gmail.com>
2026-09-02 15:28:16 +0530

    Creation of simulation page

---
+++ frontend/src/pages/Simulation.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from "react";
+import { BallManager } from "../game/classes/BallManager";
+import { WIDTH } from "../game/constants";
+import { pad } from "../game/padding";
+
+export function Simulation() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [outputs, setOutputs] = useState<{ [key: number]: number[] }>({
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+    13: [],
+    14: [],
+    15: [],
+    16: [],
+    17: [],
+  });
+  async function simulate(ballManager: BallManager) {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      ballManager.addBall(pad(WIDTH / 2 + 20 * (Math.random() - 0.5)));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ballManager = new BallManager(
+        canvasRef.current,
+        (index: number, startX?: number) => {
+          setOutputs((prevOutputs: { [key: number]: number[] }) => {
+            return {
+              ...prevOutputs,
+              [index]: [...(prevOutputs[index] || []), ...(startX !== undefined ? [startX] : [])],
+            };
+          });
+        }
+      );
+      simulate(ballManager);
+
+      return () => {
+        ballManager.stop();
+      };
+    }
+  }, [canvasRef]);
+
+  return (
+    <div className="flex flex-col lg:flex-row  items-center justify-between h-screen">
+      <div className="flex mx-16 flex-col justify-center pt-10">
+        {JSON.stringify(outputs, null, 2)}
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <canvas ref={canvasRef} width={800} height={800}></canvas>
+      </div>
+    </div>
+  );
+}
