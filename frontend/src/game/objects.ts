ba00fa49d135ddc597c3c1c2ad4d378390d902c8
Nirvan Jain <jainnirvan070@gmail.com>
2026-09-03 22:24:57 -1200

    created  game objects

--- frontend/src/game/objects.ts
+++ frontend/src/game/objects.ts
@@ -1,4 +1,5 @@
 import { HEIGHT, NUM_SINKS, WIDTH, obstacleRadius, sinkWidth } from "./constants";
+import { MULTIPLIERS } from "./multipliers";
 import { pad } from "./padding";
 
 export interface Obstacle {
@@ -13,26 +14,6 @@ export interface Sink {
     width: number;
     height: number;
     multiplier?: number;
-}
-
-const MULTIPLIERS: {[ key: number ]: number} = {
-    1: 16,
-    2: 9,
-    3: 2,
-    4: 1.4,
-    5: 1.4,
-    6: 1.2,
-    7: 1.1,
-    8: 1,
-    9: 0.5,
-    10: 1,
-    11: 1.1,
-    12: 1.2,
-    13: 1.4,
-    14: 1.4,
-    15: 2,
-    16: 9,
-    17: 16
 }
 
 export const createObstacles = (): Obstacle[] => {
@@ -59,7 +40,7 @@ export const createSinks = (): Sink[] => {
       const y = HEIGHT - 170;
       const width = sinkWidth;
       const height = width;
-      sinks.push({ x, y, width, height, multiplier: MULTIPLIERS[i+1] });
+      sinks.push({ x, y, width, height, multiplier: MULTIPLIERS[i] });
     }
 
     return sinks;
