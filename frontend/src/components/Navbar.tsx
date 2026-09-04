69c725ff58a177c06c70b2c30bfb6013dbd3408b
Nirvan Jain <jainnirvan070@gmail.com>
2026-09-03 22:23:56 -1200

    updated navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
   return (
-    <nav className="bg-white z-50 border-gray-200 dark:bg-[#262522] borbder-b shadow-lg">
+    <nav className="bg-white z-50 border-gray-200 dark:bg-[#262522] border-b shadow-lg">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           to="/"
