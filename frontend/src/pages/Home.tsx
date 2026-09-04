737ee0c9073a5d1f3375673ce16fb8aa84f8c2ca
Nirvan Jain <jainnirvan070@gmail.com>
2026-09-03 22:25:37 -1200

    updated home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,21 @@
-import { useEffect, useRef } from "react";
-import { BallManager } from "../game/classes/BallManager";
-import { WIDTH } from "../game/constants";
-import { pad } from "../game/padding";
-import { Simulate } from "../components/Simulate";
 import { Quotes, FoundIssue } from "../components";
+import { HEIGHT, WIDTH } from "../game/constants";
+import { usePlinko } from "../game/usePlinko";
 
 export function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  async function simulate(ballManager: BallManager) {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      ballManager.addBall(pad(WIDTH / 2 + 20 * (Math.random() - 0.5)));
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    }
-  }
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      const ballManager = new BallManager(
-        canvasRef.current,
-        () => {
-          // Outputs tracking
-        }
-      );
-      simulate(ballManager);
-
-      return () => {
-        ballManager.stop();
-      };
-    }
-  }, [canvasRef]);
+  const { canvasRef } = usePlinko(undefined, 1000);
 
   return (
-    <div className="">
-      <canvas ref={canvasRef} width={WIDTH} />
-      <div className="flex flex-col lg:flex-row  items-center justify-between ">
-        <Simulate />
+    <div>
+      <div className="flex justify-center">
+        <canvas
+          ref={canvasRef}
+          width={WIDTH}
+          height={HEIGHT}
+          className="max-w-full h-auto"
+        />
+      </div>
+      <div className="flex justify-center">
         <Quotes />
       </div>
       <FoundIssue />
